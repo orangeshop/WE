67ae5ff58575e0f4c41c9c3e6428c73f596488f7
최영호 <rhkeh123@naver.com>
2024-09-04 17:27:32 +0900

    ui(S11P21D104-112) : Home Account ViewPager 연결 완료

--- android/presentation/src/main/java/com/we/presentation/component/adapter/HomeViewPagerAccountAdapter.kt
+++ android/presentation/src/main/java/com/we/presentation/component/adapter/HomeViewPagerAccountAdapter.kt
@@ -1,9 +1,31 @@
 package com.we.presentation.component.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.we.presentation.databinding.ItemAccountBinding
 
-//class HomeViewPagerAccountAdapter() : RecyclerView.Adapter<HomeViewPagerAccountAdapter.ViewHolder>() {
-//    inner class HomeViewPagerAccountViewHolder(val binding: ItemHomeViewPagerAccountBinding) : RecyclerView.ViewHolder(binding.root){
-//
-//    }
-//}
+class HomeViewPagerAccountAdapter(val item: List<String>) : RecyclerView.Adapter<HomeViewPagerAccountAdapter.HomeViewPagerAccountViewHolder>() {
+    inner class HomeViewPagerAccountViewHolder(val binding: ItemAccountBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(item: String){
+//            binding.tvAccountNumber.text = item
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HomeViewPagerAccountViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemAccountBinding.inflate(inflater, parent, false)
+        return HomeViewPagerAccountViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return item.size
+    }
+
+    override fun onBindViewHolder(holder: HomeViewPagerAccountViewHolder, position: Int) {
+        holder.bind(item[position])
+    }
+}
--- android/presentation/src/main/java/com/we/presentation/home/HomeFragment.kt
+++ android/presentation/src/main/java/com/we/presentation/home/HomeFragment.kt
@@ -2,12 +2,27 @@ package com.we.presentation.home
 
 import com.we.presentation.R
 import com.we.presentation.base.BaseFragment
+import com.we.presentation.component.adapter.HomeViewPagerAccountAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import com.we.presentation.databinding.FragmentHomeBinding
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
     override fun initView() {
+        setUpViewPager()
+    }
+
+
+    private fun setUpViewPager(){
+
+        val test = arrayListOf("1","2","3")
+
+        val adapter = HomeViewPagerAccountAdapter(test)
+        binding.vpHomeAccount.adapter = adapter
+        binding.vpHomeTotalAccountDotsIndicator.attachTo(binding.vpHomeAccount)
+
 
     }
+
 }
