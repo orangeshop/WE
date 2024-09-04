79abab2ce701d21c3ba706d679b8cf9a1e02a09e
최영호 <rhkeh123@naver.com>
2024-09-04 17:34:47 +0900

    ui(S11P21D104-112) : Home Banner ViewPager 연결 완료

--- android/presentation/src/main/java/com/we/presentation/home/HomeFragment.kt
+++ android/presentation/src/main/java/com/we/presentation/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.we.presentation.home
 import com.we.presentation.R
 import com.we.presentation.base.BaseFragment
 import com.we.presentation.component.adapter.HomeViewPagerAccountAdapter
+import com.we.presentation.component.adapter.HomeViewPagerBannerAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import com.we.presentation.databinding.FragmentHomeBinding
 
@@ -10,19 +11,25 @@ import com.we.presentation.databinding.FragmentHomeBinding
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     override fun initView() {
-        setUpViewPager()
+        setUpAccountViewPager()
+        setUpBannerViewPager()
     }
 
 
-    private fun setUpViewPager(){
-
+    private fun setUpAccountViewPager(){
         val test = arrayListOf("1","2","3")
 
         val adapter = HomeViewPagerAccountAdapter(test)
         binding.vpHomeAccount.adapter = adapter
         binding.vpHomeTotalAccountDotsIndicator.attachTo(binding.vpHomeAccount)
+    }
 
+    private fun setUpBannerViewPager(){
+        val test = arrayListOf("1","2","3")
 
+        val adapter = HomeViewPagerBannerAdapter(test)
+        binding.vpHomeBanner.adapter = adapter
+        binding.vpHomeTotalBannerDotsIndicator.attachTo(binding.vpHomeBanner)
     }
 
 }
