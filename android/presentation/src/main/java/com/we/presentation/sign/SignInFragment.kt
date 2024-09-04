fdae5e2ea4de7e0ccfed475ae9591daae8275c48
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-09-04 14:49:28 +0900

    ui (#S11P21D104-89) : 로그인 UI 완료

---
+++ android/presentation/src/main/java/com/we/presentation/sign/SignInFragment.kt
@@ -0,0 +1,15 @@
+package com.we.presentation.sign
+
+import com.we.presentation.R
+import com.we.presentation.base.BaseFragment
+import com.we.presentation.databinding.FragmentSignInBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sign_in) {
+
+    override fun initView() {
+
+    }
+
+}
