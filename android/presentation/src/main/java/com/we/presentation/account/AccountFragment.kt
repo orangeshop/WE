5efbdfd9d1e898b09936f738eeb62325b5107b09
최영호 <rhkeh123@naver.com>
2024-09-06 10:17:49 +0900

    ui(S11P21D104-128) : account UI 작성 완료 (bottom sheet x)

---
+++ android/presentation/src/main/java/com/we/presentation/account/AccountFragment.kt
@@ -0,0 +1,18 @@
+package com.we.presentation.account
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.we.presentation.R
+import com.we.presentation.base.BaseFragment
+import com.we.presentation.databinding.FragmentAccountBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AccountFragment : BaseFragment<FragmentAccountBinding>(R.layout.fragment_account) {
+    override fun initView() {
+
+    }
+}
