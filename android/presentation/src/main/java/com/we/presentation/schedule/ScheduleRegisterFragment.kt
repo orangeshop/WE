6bf3fea0d71d59c5d2c89ef2681b8037c5802b0f
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-09-09 14:40:31 +0900

    ui(#S11P21D104-164) : 바텀 네비 숨기기

--- android/presentation/src/main/java/com/we/presentation/schedule/ScheduleRegisterFragment.kt
+++ android/presentation/src/main/java/com/we/presentation/schedule/ScheduleRegisterFragment.kt
@@ -10,6 +10,14 @@ class ScheduleRegisterFragment :
     BaseFragment<FragmentScheduleRegisterBinding>(R.layout.fragment_schedule_register) {
 
     override fun initView() {
+        initClickEventListener()
+    }
 
+    private fun initClickEventListener() {
+        binding.apply {
+            icTitle.ivBack.setOnClickListener {
+                navigatePopBackStack()
+            }
+        }
     }
 }
--- android/presentation/src/main/java/com/we/presentation/util/Enum.kt
+++ android/presentation/src/main/java/com/we/presentation/util/Enum.kt
@@ -1,8 +1,32 @@
 package com.we.presentation.util
 
-enum class CalendarType{
+import com.we.presentation.R
+
+enum class CalendarType {
     BEFORE,
     TODAY,
     CURRENT,
     AFTER
+}
+
+enum class Page(val hideBottomNav: Boolean) {
+    HOME(true),
+    SCHEDULE(true),
+    MY_PAGE(true),
+    SCHEDULE_REGISTER(false),
+
+    ;
+
+
+    companion object {
+        fun fromId(id: Int): Page? = entries.find { it.id == id }
+    }
+
+    val id: Int
+        get() = when (this) {
+            HOME -> R.id.fragment_home
+            SCHEDULE -> R.id.fragment_schedule
+            MY_PAGE -> R.id.fragment_my_page
+            SCHEDULE_REGISTER -> R.id.fragment_schedule_register
+        }
 }
