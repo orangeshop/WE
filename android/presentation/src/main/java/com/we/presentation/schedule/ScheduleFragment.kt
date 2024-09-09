ccaee2e941722aa2c1558ec58f6f5ec6ce5aed5a
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-09-09 13:52:36 +0900

    ui(#S11P21D104-164) : 스케줄 할일 ui

--- android/presentation/src/main/java/com/we/presentation/schedule/ScheduleFragment.kt
+++ android/presentation/src/main/java/com/we/presentation/schedule/ScheduleFragment.kt
@@ -3,6 +3,7 @@ package com.we.presentation.schedule
 import com.we.presentation.R
 import com.we.presentation.base.BaseFragment
 import com.we.presentation.component.adapter.ScheduleCalendarAdapter
+import com.we.presentation.component.adapter.ScheduleTodoAdapter
 import com.we.presentation.databinding.FragmentScheduleBinding
 import com.we.presentation.schedule.model.ScheduleUiState
 import com.we.presentation.util.CalendarType
@@ -12,16 +13,18 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ScheduleFragment : BaseFragment<FragmentScheduleBinding>(R.layout.fragment_schedule) {
     private lateinit var scheduleCalendarAdapter: ScheduleCalendarAdapter
+    private lateinit var scheduleTodoAdapter: ScheduleTodoAdapter
 
     override fun initView() {
         initScheduleCalendarAdapter()
+        initScheduleTodoAdapter()
     }
 
 
     private fun initScheduleCalendarAdapter() {
         scheduleCalendarAdapter = ScheduleCalendarAdapter()
         binding.apply {
-            rcScheduleCalendar.adapter = scheduleCalendarAdapter
+            rvScheduleCalendar.adapter = scheduleCalendarAdapter
         }
         scheduleCalendarAdapter.submitList(
             listOf(
@@ -34,5 +37,13 @@ class ScheduleFragment : BaseFragment<FragmentScheduleBinding>(R.layout.fragment
                 ScheduleUiState.ScheduleSet(CalendarType.AFTER, "1"),
             )
         )
+    }
+
+    private fun initScheduleTodoAdapter() {
+        scheduleTodoAdapter = ScheduleTodoAdapter()
+        binding.apply {
+            rvScheduleTodo.adapter = scheduleTodoAdapter
+        }
+        scheduleTodoAdapter.submitList(listOf(" ", " ", " ", " "))
     }
 }
