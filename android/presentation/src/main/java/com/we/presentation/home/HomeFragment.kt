adbd3481230fd42e4b5648c7313d1f76b394f32b
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-09-09 16:40:57 +0900

    ui(#S11P21D104-93) : 청첩장 UI 완료

--- android/presentation/src/main/java/com/we/presentation/home/HomeFragment.kt
+++ android/presentation/src/main/java/com/we/presentation/home/HomeFragment.kt
@@ -1,26 +1,26 @@
 package com.we.presentation.home
 
-import android.util.Log
 import com.we.presentation.R
 import com.we.presentation.base.BaseFragment
 import com.we.presentation.component.adapter.HomeViewPagerAccountAdapter
 import com.we.presentation.component.adapter.HomeViewPagerBannerAdapter
-import dagger.hilt.android.AndroidEntryPoint
 import com.we.presentation.databinding.FragmentHomeBinding
-import timber.log.Timber
+import dagger.hilt.android.AndroidEntryPoint
 
 private const val TAG = "HomeFragment_싸피"
+
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     override fun initView() {
         setUpAccountViewPager()
         setUpBannerViewPager()
+        initClickEventListener()
     }
 
 
-    private fun setUpAccountViewPager(){
-        val test = arrayListOf("1","2","3")
+    private fun setUpAccountViewPager() {
+        val test = arrayListOf("1", "2", "3")
 
         val adapter = HomeViewPagerAccountAdapter(test, accountClickListener = {
             navigateDestination(R.id.action_fragment_home_to_accountFragment)
@@ -32,13 +32,22 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
         }
     }
 
-    private fun setUpBannerViewPager(){
-        val test = arrayListOf("1","2","3")
+    private fun setUpBannerViewPager() {
+        val test = arrayListOf("1", "2", "3")
 
         val adapter = HomeViewPagerBannerAdapter(test)
         binding.apply {
             vpHomeBanner.adapter = adapter
             vpHomeTotalBannerDotsIndicator.attachTo(vpHomeBanner)
+        }
+    }
+
+
+    private fun initClickEventListener() {
+        binding.apply {
+            icInvitation.flContent.setOnClickListener {
+                navigateDestination(R.id.action_fragment_home_to_fragment_invitation)
+            }
         }
     }
 
