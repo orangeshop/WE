fefe2f58f58a0c72ed718c7e8e4f5ad2706d20e6
권윤하 <ellen4421@naver.com>
2024-09-11 11:16:15 +0900

    Chore : instance.ts 서버 경로 변경

--- WE_project/src/apis/utils/instance.ts
+++ WE_project/src/apis/utils/instance.ts
@@ -1,14 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.100.149:8080/";
+const BASE_URL = "http://192.168.100.149:8080/v1/";
 
 const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
 });
-
-// export const clearTokenInterceptors = (interceptorId: number) => {
-//   api.interceptors.request.eject(interceptorId);
-// };
 
 export default api;
