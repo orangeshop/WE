36638677431d0a0fcd30988d93f9e64fc55fd495
권윤하 <ellen4421@naver.com>
2024-09-13 10:21:48 +0900

    Feat : 샘플보기 라우팅 설정

---
+++ WE_project/src/MobileInvitation/FreeTypeSample.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FreeTypeSample: React.FC = () => {
+  return (
+    <div>
+      <h1>자유형 청첩장 샘플</h1>
+    </div>
+  );
+};
+
+export default FreeTypeSample;
--- WE_project/src/apis/utils/instance.ts
+++ WE_project/src/apis/utils/instance.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.100.149:8080/v1";
+// const BASE_URL = "http://192.168.100.149:8080/v1";
+
+const BASE_URL = "http://192.168.100.95:8080/v1";
 
 const api = axios.create({
   baseURL: BASE_URL,
