fda81a1c7137f1052b4f1e40d6810f61c31695b8
insuKang <kin4496@naver.com>
2024-09-19 10:16:54 +0900

    feat : cors 설정 파일 추가

--- we-springboot/src/main/java/com/akdong/we/WeApplication.java
+++ we-springboot/src/main/java/com/akdong/we/WeApplication.java
@@ -2,8 +2,9 @@ package com.akdong.we;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class WeApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeApplication.class, args);
