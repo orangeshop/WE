5ad0d31af7c26a0f301b01efb70001a86dc9ef5c
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-09-19 12:55:53 +0900

    feat : 회원가입 완료시 로직 처리

--- android/presentation/src/main/java/com/we/presentation/sign/SignUpSuccessFragment.kt
+++ android/presentation/src/main/java/com/we/presentation/sign/SignUpSuccessFragment.kt
@@ -1,24 +1,22 @@
 package com.we.presentation.sign
 
-import android.content.Intent
 import com.we.presentation.R
 import com.we.presentation.base.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
 import com.we.presentation.databinding.FragmentSignUpSuccessBinding
-import com.we.presentation.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SignUpSuccessFragment : BaseFragment<FragmentSignUpSuccessBinding>(R.layout.fragment_sign_up_success) {
+class SignUpSuccessFragment :
+    BaseFragment<FragmentSignUpSuccessBinding>(R.layout.fragment_sign_up_success) {
 
     override fun initView() {
         initClickEvent()
     }
 
-    private fun initClickEvent(){
+    private fun initClickEvent() {
         binding.apply {
-            tvSignSuccessComplete.setOnClickListener{
-                startActivity(Intent(requireActivity(), MainActivity::class.java))
-                requireActivity().finish()
+            tvSignSuccessComplete.setOnClickListener {
+                navigatePopBackStack()
             }
         }
     }
