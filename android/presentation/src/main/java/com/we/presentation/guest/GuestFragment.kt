8e17fbdc273c846a09b96fb67efedbcc12b40ab8
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-09-19 16:26:33 +0900

    ui(#S11P21D104-163) : 하객용 UI

---
+++ android/presentation/src/main/java/com/we/presentation/guest/GuestFragment.kt
@@ -0,0 +1,13 @@
+package com.we.presentation.guest
+
+import com.we.presentation.R
+import com.we.presentation.base.BaseFragment
+import com.we.presentation.databinding.FragmentGuestBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class GuestFragment : BaseFragment<FragmentGuestBinding>(R.layout.fragment_guest) {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+}
