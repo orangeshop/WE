1395fd7e4f106c520eb3ffc0337d9df9395fd135
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-09-23 18:01:28 +0900

    feat : 바텀 시트

---
+++ android/presentation/src/main/java/com/we/presentation/schedule/viewmodel/ScheduleRegisterViewModel.kt
@@ -0,0 +1,15 @@
+package com.we.presentation.schedule.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ScheduleRegisterViewModel @Inject constructor(
+
+) : ViewModel() {
+
+
+
+
+}
