0d73c682c35c9fd0f669c0bf3a49b32728d16750
최영호 <rhkeh123@naver.com>
2024-09-24 13:00:58 +0900

    feat (S11P21D104-316) : 1원 송금 인증 로직 작성 완료 (FCM x)

--- android/presentation/src/main/java/com/we/presentation/account/AccountTransferFragment.kt
+++ android/presentation/src/main/java/com/we/presentation/account/AccountTransferFragment.kt
@@ -5,42 +5,73 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import com.we.presentation.R
+import com.we.presentation.account.viewmodel.AccountViewModel
 import com.we.presentation.base.BaseFragment
 import com.we.presentation.databinding.FragmentAccountTransferBinding
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
-class AccountTransferFragment : BaseFragment<FragmentAccountTransferBinding>(R.layout.fragment_account_transfer) {
+class AccountTransferFragment :
+    BaseFragment<FragmentAccountTransferBinding>(R.layout.fragment_account_transfer) {
+    private val accountViewModel: AccountViewModel by activityViewModels()
     override fun initView() {
         backBtnClickListener()
         autoFocusMove()
+        initAuthCode()
     }
 
-    private fun autoFocusMove(){
+    private fun initAuthCode() {
+        binding.apply {
+            tvRegisterAccount.setOnClickListener {
+                accountViewModel.setAuthCode(
+                    etTransferNumber1.text.toString(),
+                    etTransferNumber2.text.toString(),
+                    etTransferNumber3.text.toString(),
+                    etTransferNumber4.text.toString()
+                )
+                accountViewModel.getAuthCodeCertified() { result ->
+                    if (result == true) {
+                        Toast.makeText(requireContext(), "성공", Toast.LENGTH_SHORT).show()
+                        navigatePopBackStack()
+                    } else {
+                        Toast.makeText(requireContext(), "잘못된 접근입니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
+        }
+    }
+
+    private fun autoFocusMove() {
         binding.apply {
             etTransferNumber1.addTextChangedListener {
-                if("1".equals(it.toString())){
+                if (etTransferNumber1.text.isNotBlank()) {
                     etTransferNumber2.requestFocus()
                 }
             }
 
             etTransferNumber2.addTextChangedListener {
-                if("1".equals(it.toString())){
+                if (etTransferNumber2.text.isNotBlank()) {
                     etTransferNumber3.requestFocus()
                 }
             }
 
             etTransferNumber3.addTextChangedListener {
-                if("1".equals(it.toString())){
+                if (etTransferNumber3.text.isNotBlank()) {
                     etTransferNumber4.requestFocus()
                 }
             }
         }
     }
 
-    private fun backBtnClickListener(){
+    private fun backBtnClickListener() {
         binding.apply {
             ivAccountBack.setOnClickListener {
                 navigatePopBackStack()
