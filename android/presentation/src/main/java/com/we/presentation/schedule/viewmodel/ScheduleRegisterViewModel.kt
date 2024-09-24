18397cf258c64afd105d2c4602c55163b543b7ca
ho-taek <71322949+ho-taek@users.noreply.github.com>
2024-09-24 14:42:41 +0900

    feat(#S11P21D104-290) : 등록 파라미터 지정

--- android/presentation/src/main/java/com/we/presentation/schedule/viewmodel/ScheduleRegisterViewModel.kt
+++ android/presentation/src/main/java/com/we/presentation/schedule/viewmodel/ScheduleRegisterViewModel.kt
@@ -1,7 +1,17 @@
 package com.we.presentation.schedule.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.we.model.ScheduleParam
+import com.we.presentation.util.ScheduleRegisterType
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,6 +20,61 @@ class ScheduleRegisterViewModel @Inject constructor(
 ) : ViewModel() {
 
 
+    private val _scheduleRegisterParam = MutableStateFlow<ScheduleParam>(ScheduleParam())
+    val scheduleRegisterParam: StateFlow<ScheduleParam> get() = _scheduleRegisterParam
 
+    fun setRegisterParam(type: ScheduleRegisterType, content: Any?) {
+        when (type) {
+            ScheduleRegisterType.DATE -> {
+                _scheduleRegisterParam.update {
+                    it.copy(
+                        date = content.toString()
+                    )
+                }
+            }
 
+            ScheduleRegisterType.PRICE -> {
+                if (content != null) {
+                    _scheduleRegisterParam.update {
+                        it.copy(
+                            price = content as Long
+                        )
+                    }
+                }
+            }
+
+            ScheduleRegisterType.CONTENT -> {
+                _scheduleRegisterParam.update {
+                    it.copy(
+                        content = content.toString()
+                    )
+                }
+            }
+
+            ScheduleRegisterType.LOCATION -> {
+                _scheduleRegisterParam.update {
+                    it.copy(
+                        location = content.toString()
+                    )
+                }
+            }
+        }
+    }
+
+    private val _registerButtonActive = MutableSharedFlow<Boolean>(1)
+    val registerButtonActive: SharedFlow<Boolean> get() = _registerButtonActive
+
+    fun setRegisterButtonActive() {
+        viewModelScope.launch {
+            _scheduleRegisterParam.collectLatest {
+                val result =
+                    it.content.isNotEmpty() && it.location.isNotEmpty() && it.date.isNotEmpty()
+                _registerButtonActive.emit(result)
+            }
+        }
+    }
+
+    init {
+        setRegisterButtonActive()
+    }
 }
